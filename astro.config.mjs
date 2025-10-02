c6352dc49bdae08021d0eeace30d413156db2971
Filip Nowakowski <fnowakow@cisco.com>
2025-10-02 13:55:44 +0200

    feat: tailwind.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://somebody16.github.io',
+  site: 'https://somebody16.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
